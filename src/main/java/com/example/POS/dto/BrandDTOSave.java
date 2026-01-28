274b80b1be5861665801ca7a4c90a0ab3bbeeffe
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-01-28 17:43:47 -0300

    criação dos DTOs para Brand

---
+++ src/main/java/com/example/POS/dto/BrandDTOSave.java
@@ -0,0 +1,13 @@
+package com.example.POS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BrandDTOSave {
+    private String brandName;
+    private boolean active;
+}
